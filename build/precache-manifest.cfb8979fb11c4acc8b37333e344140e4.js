f75f47709ce1c61fe5fd42ac1b66df035d3563df
Supratik Das <rick.das08@gmail.com>
2020-06-28 04:14:07 +0530

    build commit

---
+++ build/precache-manifest.cfb8979fb11c4acc8b37333e344140e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bab230ded88185a303481b5df5d03e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3011744763a8fbd537ab",
+    "url": "/static/js/2.72bc9e73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72bc9e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a106481667531db920b",
+    "url": "/static/js/main.8d1920b5.chunk.js"
+  },
+  {
+    "revision": "882702964693dbdf34c8",
+    "url": "/static/js/runtime-main.99de8262.js"
+  },
+  {
+    "revision": "53c7454e4666fd183fc0c654a2195ee5",
+    "url": "/static/media/twitter.53c7454e.svg"
+  }
+]);
