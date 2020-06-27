afa267ba29d2b529ea15654d4efe2559759c00b7
palakg11 <32808919+palakg11@users.noreply.github.com>
2020-06-27 19:15:49 +0530

    Add campaignform

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ import {
 import { Input,useDisclosure } from "@chakra-ui/core";
 import Twitter from "./twitter.svg"
 import { List, ListItem, ListIcon } from "@chakra-ui/core";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/core";
+import { Textarea } from "@chakra-ui/core";
+import { Select } from "@chakra-ui/core";
 
 function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -186,7 +194,7 @@ function App1() {
             {console.log(items)}
             {items.map(campaign => (
               <Box>
-                <Button w="100%"leftIcon="check-circle" color="green.500" >{campaign.name}</Button>
+                <Button mb="0.1em" w="100%"leftIcon="check-circle" color="green.500" size="lg">{campaign.name}</Button>
               </Box>
             ))}
             </Box>
@@ -237,5 +245,118 @@ function App1() {
 }
 
 
+function CampaignForm() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  return (
+    <ThemeProvider>
+   <CSSReset />
+      <Box w="100vw" h="100vh" d="flex" flexDirection="row">
+          <Box h= "100%" w= "20%" d="flex" flexDirection="column">
+            <Box position="static" p="5%" w="100%" d="flex" justifyContent= "center"  border="1px" borderRadius="md" borderColor="gray.200">
+              My Campaigns
+            </Box>
+            <Box d="flex" flexDirection="column" w="100%">
+            {/* list of all campings */}
+            <Box>
+            <Button w="100%"leftIcon="check-circle" color="green.500" size="lg">Campaign 1</Button>
+            </Box>
+            <Box mt="0.1em">
+            <Button w="100%"leftIcon="check-circle" color="green.500" size="lg">Campaign 2</Button>
+            </Box>
+            <Box mt="0.1em">
+            <Button w="100%"leftIcon="check-circle" color="green.500" size="lg">Campaign 3</Button>
+            </Box>
+            <Box mt="0.1em">
+            <Button w="100%"leftIcon="check-circle" color="green.500" size="lg">Campaign 4</Button>
+            </Box>
+            </Box>
+          </Box>
+          <Divider mr="0" ml="0" orientation="vertical" />
+          <Box h= "100%" w="80%" d="flex" flexDirection="column" alignItems= "center">
+            <Box width="100%" display="flex" justifyContent="space-between" pr="1em" pl="1em" mt="2em">
+            <Box d="flex" flexDirection="row">
+              <Box><Button>Save</Button></Box>
+              <Box ml="1em"><Button>Refresh</Button></Box>
+            </Box>
+            <Box>
+              <Button ref={btnRef} onClick={onOpen} leftIcon="add">
+                New Campaign
+              </Button>
+              </Box>
+            <Drawer isOpen={isOpen} placement="top" onClose={onClose} finalFocusRef={btnRef}>
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Campaign Name</DrawerHeader>
+
+                <DrawerBody>
+                  <Input placeholder="Type here..." />
+                </DrawerBody>
+
+                <DrawerFooter>
+                  <Button variant="outline" mr={3} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button color="blue">Save</Button>
+                </DrawerFooter>
+              </DrawerContent>
+            </Drawer>
+            </Box>
+            <Box d="flex" flexDirection="column" justifyContent = "flex-start" alignItems= "center" w="100%" mt="2em">
+            <form style={{width : '100%'}}>
+              <FormControl isRequired>
+              <Box w="100%" d="flex" flexDirection="row" justifyContent="space-between">
+                <Box w="20%" pl="1em"><FormLabel htmlFor="name">Campaign Name</FormLabel></Box>
+                <Box w="70%" pr="1em"><Input id="name" name="name" placeholder="Campaign Name" /></Box>
+              </Box>
+              </FormControl>
+              <FormControl isRequired>
+              <Box w="100%" d="flex" flexDirection="row" justifyContent="space-between" mt="1em">
+                <Box w="20%" pl="1em"><FormLabel htmlFor="message">Message Text</FormLabel></Box>
+                <Box w="70%" pr="1em">
+                <Textarea name="message" id="message" placeholder="Type your message here....." />
+                </Box>
+              </Box>
+              </FormControl>
+              <FormControl isRequired>
+              <Box w="100%" d="flex" flexDirection="row" justifyContent="space-between" mt="1em">
+                <Box w="20%" pl="1em"><FormLabel htmlFor="strategy">Strategy</FormLabel></Box>
+                <Box w="70%" pr="1em">
+                  <Box w="30%">
+                    <Select placeholder="Select strategy" id="strategy" name="strategy">
+                      <option value="option1">strategy 1</option>
+                      <option value="option2">strategy 2</option>
+                      <option value="option3">strategy 3</option>
+                    </Select>
+                  </Box>
+                </Box>
+              </Box>
+              </FormControl>
+            </form>
+            <Box d="flex" flexDirection="column" justifyContent = "flex-start" alignItems= "center" w="100%" mt="2em">
+              <Box w="100%" d="flex" flexDirection="row" justifyContent="space-between">
+                <Box w="20%" pl="1em">Status</Box>
+                <Box w="70%" pr="1em">0/2</Box>
+              </Box>
+            </Box>
+            </Box>
+            <Box width="100%" display="flex" justifyContent="space-between" pr="1em" pl="1em" mt="20em">
+              <Box>
+                <Button>Delete Campaign</Button>
+              </Box>
+              <Box d="flex" flexDirection="row">
+                <Box><Button>Reset</Button></Box>
+                <Box ml="1em"><Button>Pause</Button></Box>
+                <Box ml="1em"><Button>Start Campaign</Button></Box>
+            </Box>
+            </Box>
+          </Box>
+      </Box>
+      </ThemeProvider>
+  );
+}
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App1 />, rootElement);
+ReactDOM.render(<CampaignForm />, rootElement);
