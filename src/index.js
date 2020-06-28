c842dc170d6a339e46f57b0c8aa01659c01d0dba
Supratik Das <rick.das08@gmail.com>
2020-06-28 18:58:55 +0530

    images

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ import {
   DrawerCloseButton,
 } from "@chakra-ui/core";
 import { Input,useDisclosure } from "@chakra-ui/core";
-import Logo from "./logo.png"
-import Dummy from "./dummy.png"
+import Twitter from "./twitter.svg"
 import { List, ListItem, ListIcon } from "@chakra-ui/core";
 import {
   FormControl,
@@ -160,8 +159,6 @@ function App() {
           }
         )
       }
-      // setMe({ name: "palak", handle: "palak_goenka", followers: 34, following: 67, id: 98 })
-      fetchCampaigns(result.user_id);
     }, [])
 
     function showCampaign(name) { 
@@ -244,10 +241,10 @@ function App() {
   return (
     <ThemeProvider>
    <CSSReset />
-      <Box w="100vw" h="100vh" d="flex" flexDirection="row" bg="grey.50">
+      <Box w="100vw" h="100vh" d="flex" flexDirection="row">
           <Box h= "100%" w= "20%" d="flex" flexDirection="column">
             <Box position="static" p="5%" w="100%" d="flex" justifyContent= "center"  border="1px" borderRadius="md" borderColor="gray.200">
-            <Heading as="h3" size="lg">My Campaigns</Heading>
+              My Campaigns
             </Box>
             <Box d="flex" flexDirection="column" w="100%">
             {/* list of all campings */}
@@ -338,33 +335,21 @@ function App() {
                 </form>
               </Box> 
             ) : 
-              <Box d="flex" width="100%">
-                <Box d="flex" width="100%"> { currentCampaign == null ? (
-                  <Box d="flex" flexDirection="column" justifyContent = "center" alignItems= "center" mt="8em" w="100%">
-                  {/* <Box d="flex" w="100%" justifyContent = "center">
-                    <Image size="40%" justifyContent = "center" src={Logo} alt="App logo" />
-                  </Box> */}
+              <Box>
+                <Box> { currentCampaign == null ? (
+                  <Box d="flex" flexDirection="column" justifyContent = "center" alignItems= "center" mt="8em">
+                  <Box d="flex" w="100%" justifyContent = "center">
+                    <Image size="20%" justifyContent = "center" src={Twitter} alt="Twitter logo" />
+                  </Box>
                   { me ? (
-                  <Box d="flex" w="100%" justifyContent= "center" flexDirection="column">
-                    <Box d="flex" w="100%" justifyContent = "center">
-                      <Image justifyContent = "center" src={Dummy} alt="App logo" />
-                    </Box>
-                    <Box d="flex" justifyContent = "center" w="100%" mt="2em">
-                      <Heading as="h4" size="md">{me.name}</Heading>
-                    </Box>
-                    <Box d="flex" justifyContent = "center" w="100%" mt="0.5em">
-                      <Heading as="h3" size="lg">@{me.handle}</Heading>
-                      </Box>
+                  <Box> 
+                    <Box d="flex" justifyContent = "center" w="100%" mt="2em">{me.name}</Box>
+                    <Box d="flex" justifyContent = "center" w="100%" mt="0.5em">@{me.handle}</Box>
                       <Box d="flex" flexDirection="row" justifyContent = "center" w="100%" mt="0.5em">
-                        <Box><Heading as="h4" size="sm">{me.following} Following </Heading></Box>
-                        <Box ml="4%"><Heading as="h4" size="sm">{me.followers} Followers </Heading></Box>
+                        <Box>{me.following} Following</Box>
+                        <Box ml="4%">{me.followers} Followers</Box>
                       </Box>
-                    </Box>)  : (
-                      <Box d="flex" w="100%" justifyContent = "center" flexDirection = "column" alignItems="center">
-                        <Box d="flex" justifyContent="center"><Image size="40%" justifyContent = "center" src={Logo} alt="App logo" /></Box>
-                        <Box mt="1em"><Heading as="h3" size="md">v1.0</Heading></Box>
-                      </Box>
-                    )}
+                    </Box>)  : ''}
                   </Box>
                 ): (<Box> { me ? 
                       <Box>
